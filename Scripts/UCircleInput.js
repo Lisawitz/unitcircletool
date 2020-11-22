40a2a552c1b4ab61609816c499601cf0a0ef9e7a
Lisawitz <51464286+Lisawitz@users.noreply.github.com>
2020-11-21 20:42:37 -0800

    Update UCircleInput.js

--- Scripts/UCircleInput.js
+++ Scripts/UCircleInput.js
@@ -17,10 +17,8 @@ function getDeg(rawInAngle)
 function getRad(rawInAngle)
 {
     const radiansRgx = /^-?[0-9]*(pi)?((\/)-?[0-9]+)?$/;
-    console.log('input was ', rawInAngle);
     if (!radiansRgx.test(rawInAngle))
     {
-        console.log('fail');
         return;
     }
 
@@ -44,17 +42,11 @@ function parseRad(rawInAngle)
         numerator = rawInAngle;
         denominator = 1;
     }
-    console.log('got a numerator of ', numerator);
-    console.log('got a denominator of ', denominator);
 
     var num = parseInt(numerator.replace('pi', ''));
     var den = parseInt(denominator);
 
-    console.log('num = ', num);
-    console.log('den = ', den);
-
     inAngle = (num * 180) / den;
-    console.log('got ', inAngle);
     
     if(inAngle > 1800 || inAngle < -1800)
     {
